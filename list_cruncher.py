c9148496dc5e06a30227ed07fc40af5c7332f489
Christopher Woodall <chris.j.woodall@gmail.com>
2010-12-16 14:12:31 -0500

    Added functionality to export spreadsheet from Google Docs to a file_path.
    Need to be able to work with csv files now to do file processing

--- list_cruncher.py
+++ list_cruncher.py
@@ -1,40 +1,84 @@
 #!/usr/bin/python
-import gdata.docs.data
-import gdata.docs.client
+##
+# Name: List Cruncher
+# Purpose: Crunch lists for OneThirtyBPMs end of year list
+# Date: December 16, 2010
+#
+# Developer: Christopher Woodall <chris.j.woodall at gmail.com>
+# Copyright: MIT/BSD License
+##
+
+import gdata.docs
+import gdata.docs.service
 import gdata.service
+import gdata.spreadsheet.service
 
-# set the metadata for the program connecting to the google docs server
-company = 'onethirtybpm'
-app = 'listcruncher'
-version = 'v1'
-application = company + '-' + app + '-' + version
+# Print feed information to the console
+def PrintFeed(feed):
+	'''Prints out the contents of a feed to the console.'''
+	print '\n'
+	if not feed.entry:
+		print 'No entries in feed.\n'
+	for entry in feed.entry:
+		print '%s' % (entry.title.text.encode('UTF-8'))			
 
-def setup_gdocs(application_string, ssl=True, debug=False):
-	'''Setup connection to gdocs'''
-	client = gdata.docs.client.DocsClient(source=application_string)
-	client.ssl = ssl
-	client.http_client.debug = False
-	return client
-	
+
 def main():
-	# initialize gd_client
-	gd_client = setup_gdocs(application)
+	# set the metadata for the program connecting to the google docs server
+	company = 'onethirtybpm'
+	app = 'listcruncher'
+	version = 'v1'
+	# Produce the source_string/application name sent to google for connection
+	application = company + '-' + app + '-' + version
 	
+	# Input
 	email = raw_input("Email: ")
 	password = raw_input("Password: ")
 	
-	# handle captcha challenges
+	# -DEPRECIATED: silly because function is  a oneliner. Just include
+#	gd_client = SetupClient(email, password, application) # initialize gd_client
+		
+	# Initialize DocService class on client gd_client
+	gd_client = gdata.docs.service.DocsService(source=application)
+	spreadsheets_client = gdata.spreadsheet.service.SpreadsheetsService()
+	
+	# Handle captcha challenges on login
 	try:
-		gd_client.ClientLogin(email, password, source=application)
+		gd_client.ClientLogin(email, password)
 	except gdata.service.CaptchaRequired:
 		print('Please visit ' + gd_client.captcha_url)
 		answer = raw_input('Answer to the challenge? ')
 		gd_client.ClientLogin(email, password, source=application,
 			captcha_token=client.captcha_token, captcha_response=answer)
-	except gdata.client.BadAuthentication:
+	except gdata.service.BadAuthentication:
 		exit('Users credentials were unrecognized')
-	except gdata.client.Error:
+	except gdata.service.Error:
 		exit('Login Error')
+	
+	document_title = ''
+	q = gdata.docs.service.DocumentQuery()
+	while not document_title:
+		# Query for spreadsheet
+		document_title = raw_input('Spreadsheet (Press ENTER for a list of spreadsheets): ')	
+		if not document_title:
+			q.categories=['spreadsheet']
+		else:
+			q['title'] = document_title
+			q['title-exact'] = 'true'
+		feed = gd_client.Query(q.ToUri())	
+		PrintFeed(feed)
+	
+	file_path = raw_input('File Path: ')
+	
+	# Login to spreadsheet
+	spreadsheets_client.ClientLogin(email, password)
+	# substitute the spreadsheets token into our gd_client
+	docs_auth_token = gd_client.GetClientLoginToken()
+	gd_client.SetClientLoginToken(spreadsheets_client.GetClientLoginToken())
+	
+	for entry in feed.entry:
+		gd_client.Export(entry, file_path, gid=0)
+		print "Downloading spreadsheet to %s" % (file_path)
 		
 if __name__ == '__main__':
 	main()
