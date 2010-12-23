ddea038c92449fa6835a26ff05cfd2c452258f81
cwoodall <chris.j.woodall@gmail.com>
2010-12-22 22:26:51 -0500

    filter function filter AlbumString added

--- csv_analysis.py
+++ csv_analysis.py
@@ -4,11 +4,11 @@ import csv
 from operator import itemgetter
 
 ## CURENT STATUS: FAIL
-class Album(object):
-	'''Album object: REVISE DOCSTRING'''
-	def __init__(self, title, votes=0):
-		self.title = title
-		self.votes = votes
+#class Album(object):
+#	'''Album object: REVISE DOCSTRING'''
+#	def __init__(self, title, votes=0):
+#		self.title = title
+#		self.votes = votes
 	
 class Voter(object):
 	# Revise DOCSTRING
@@ -17,7 +17,26 @@ class Voter(object):
 		self.name = name
 		self.multiplier = multiplier
 		self.albums = albums
-		
+
+def filterAlbumString(album_string):
+	# for this situation only
+	album_string = album_string.replace('Beatiful', 'Beautiful')
+	album_string = album_string.replace('Deehunter', 'Deerhunter')
+	album_string = album_string.replace('Monae, Janelle', 'Janelle Monae' )
+	
+	# General cleanup
+	album_string = album_string.replace('\xe2\x80\x93', '-')
+	album_string = album_string.replace('?', '')
+	album_string = album_string.replace('\t', '')
+	album_string = album_string.replace('The', '')
+	album_string = album_string.replace('  ', ' ')
+	album_string = album_string.title()
+	album_string = album_string.split(':')[0]
+	album_string = album_string.rstrip()	
+	album_string = album_string.lstrip()		
+	
+	return album_string
+				
 if __name__ == '__main__':
 	reader = csv.reader(open("test.csv", "rb"))
 	voter_list = []
@@ -29,20 +48,7 @@ if __name__ == '__main__':
 	for voter in voter_list:
 		vote_score = 50
 		for album in voter.albums:
-			album = album.replace('\xe2\x80\x93', '-')
-			album = album.replace('?', '')
-			album = album.replace('Beatiful', 'Beautiful')
-			album = album.replace('\t', '')
-			album = album.replace('Deehunter', 'Deerhunter')
-			album = album.replace('The', '')
-			album = album.replace('  ', ' ')
-			album = album.replace('Monae, Janelle', 'Janelle Monae' )
-			album = album.title()
-			album = album.split(':')[0]
-			album = album.rstrip()	
-			album = album.lstrip()		
-#			album = album.replace(' ','')
-			
+			album = filterAlbumString(album)
 			if album:
 				try:
 					album_list[album] = int(album_list[album] + (int(voter.multiplier) * int(vote_score)))
@@ -50,7 +56,8 @@ if __name__ == '__main__':
 					album_list[album] = int(voter.multiplier) * int(vote_score)
 			vote_score -= 1
 	
+	# Print out a sorted table of votes and albums
 	print "Votes | Album"
 	print "------|-------------------------------------------------------------"		
-	for album in sorted(album_list.items(), key=itemgetter(1)):
+	for album in reversed(sorted(album_list.items(), key=itemgetter(1))):
 		print "%5s | %s" % (album[1], album[0])
